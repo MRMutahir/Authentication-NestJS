9bcdcc193d3ce3e679ccf0024381a5d9d0aea6ff
MRMutahir <mutahirkareem820@gmail.com>
2024-08-13 16:07:56 +0500

    refresh token API with guard compelete

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -38,11 +38,31 @@ export class AuthService {
 
   async validateUser(dto: LoginDto) {
     const user = await this.userService.findByEmail(dto.username);
-    console.log('user', user);
     if (user && (await compare(dto.password, user.password))) {
       const { password, ...result } = user;
       return result;
     }
     throw new UnauthorizedException('Username or password is incorrect');
+  }
+
+  async refreshToken(user: any): Promise<any> {
+    const payload = {
+      username: user.username,
+      sub: user.sub,
+    };
+    // Adjust the expiration timestamp to US time
+    const expirationTimestamp = new Date().getTime() + 7 * 24 * 60 * 60 * 1000; // Adds 7 days
+
+    return {
+      accessToken: await this.jwtService.signAsync(payload, {
+        expiresIn: '20s',
+        secret: process.env.jwtSecretKey,
+      }),
+      refreshToken: await this.jwtService.signAsync(payload, {
+        expiresIn: '7d',
+        secret: process.env.jwtRefreshTokenKey,
+      }),
+      expiresIn: expirationTimestamp, // Adjusted expiration timestamp
+    };
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { AuthGuard } from 'src/auth/guards/jwt.guard';
+import { Roles } from 'src/auth/guards/roles.decorator';
 
 @Controller('user')
+@UseGuards(AuthGuard)
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
   @Get(':id')
+  @Roles(['admin'])
   async getProfile(@Param('id') id: number) {
-   return await  this.userService.findById(id);
+    return await this.userService.findById(id);
   }
 }
